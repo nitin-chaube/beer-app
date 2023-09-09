f645cea43f54038443eefcd525cc25a4fdfeab3f
Nitin Chaube <119601248+nitin-chaube@users.noreply.github.com>
2023-09-09 14:43:49 +0530

    Aother Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// src/App.js
 import React from 'react';
 import './App.css';
 import Home from './screens/Home';
-import Header from './components/Header'; // Import the Header component
+import Header from './components/Headers';
 
 function App() {
   return (
