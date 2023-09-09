fc9b2e4203a5b3f06ded4898e89064d77bd58221
Nitin Chaube <119601248+nitin-chaube@users.noreply.github.com>
2023-09-09 14:42:42 +0530

    Another Commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import './App.css';
 import Home from './screens/Home';
+import Header from './components/Header'; // Import the Header component
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
     </div>
   );
